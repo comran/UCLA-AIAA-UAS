f814b036daf7cd74fe58402ff9afa04ba628455a
Comran Morshed <comranmorsh@gmail.com>
2017-05-24 17:00:37 -0700

    Fix some formatting and 80-char stuff...

--- vision/python/gps.py
+++ vision/python/gps.py
@@ -1,17 +1,17 @@
 #! /usr/bin/python
 # Written by Dan Mandle http://dan.mandle.me September 2012
 # License: GPL 2.0
- 
+
 import os
 from gps import *
 from time import *
 import time
 import threading
- 
+
 gpsd = None #seting the global variable
- 
+
 os.system('clear') #clear the terminal (optional)
- 
+
 class GpsPoller(threading.Thread):
   def __init__(self):
     threading.Thread.__init__(self)
@@ -19,12 +19,12 @@ class GpsPoller(threading.Thread):
     gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
     self.current_value = None
     self.running = True #setting the thread running to true
- 
+
   def run(self):
     global gpsd
     while gpsp.running:
       gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
- 
+
 if __name__ == '__main__':
   gpsp = GpsPoller() # create the thread
   try:
@@ -32,9 +32,9 @@ if __name__ == '__main__':
     while True:
       #It may take a second or two to get good data
       #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
- 
+
       os.system('clear')
- 
+
       print
       print ' GPS reading'
       print '----------------------------------------'
@@ -52,9 +52,9 @@ if __name__ == '__main__':
       print 'mode        ' , gpsd.fix.mode
       print
       print 'sats        ' , gpsd.satellites
- 
+
       time.sleep(5) #set to whatever
- 
+
   except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
     print "\nKilling Thread..."
     gpsp.running = False
--- vision/python/photographer.py
+++ vision/python/photographer.py
@@ -7,30 +7,30 @@ import time
 
 class Photographer:
     def take_pictures(self, save_directory, photos):
-	i = 0
-	while 1:
-		try:
-			# Take pictures forever.
-			camera = SonyAPI()
-			camera.QX_ADDR = "http://192.168.122.1:8080"
+        i = 0
+        while 1:
+            try:
+                # Take pictures forever.
+                camera = SonyAPI()
+                camera.QX_ADDR = "http://192.168.122.1:8080"
 
-			# TODO(comran): Try/catch the below command to detect if we are actually
-			#               connected to the camera.
+                # TODO(comran): Try/catch the below command to detect if we are
+                #               actually connected to the camera.
 
-			camera.actTakePicture()
+                camera.actTakePicture()
 
-			print "Starting to take pictures..."
-			while 1:
-			    url = camera.actTakePicture()['result'][0][0]
-			    url = url.replace("\/", "/")
-			    url = url.replace("Scn", "Origin")
+                print "Starting to take pictures..."
+                while 1:
+                    url = camera.actTakePicture()['result'][0][0]
+                    url = url.replace("\/", "/")
+                    url = url.replace("Scn", "Origin")
 
-			    image_path = save_directory + "/" + str(i).zfill(5) + ".jpg"
-			    urllib.urlretrieve(url, image_path)
-			    photos.append(image_path)
+                    image_path = save_directory + "/" + str(i).zfill(5) + ".jpg"
+                    urllib.urlretrieve(url, image_path)
+                    photos.append(image_path)
 
-			    i = i + 1;
-			    print "Picture #" + str(i) + " stored at " + image_path
-		except:
-			print "Error accessing camera."
-			time.sleep(1)
+                    i = i + 1;
+                    print "Picture #" + str(i) + " stored at " + image_path
+            except:
+                print "Error accessing camera."
+                time.sleep(1)
